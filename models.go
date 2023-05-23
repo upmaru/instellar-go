c82e9e142cdca43146feacb6aba7a3d5adabd8e3
Zack Siri <me@zacksiri.dev>
2023-05-23 21:02:16 +0700

    update cluster model

--- models.go
+++ models.go
@@ -5,7 +5,11 @@ type Cluster struct {
 		Attributes struct {
 			Id           int    `json:"id"`
 			CurrentState string `json:"current_state"`
+			Name         string `json:"name"`
 			Slug         string `json:"slug"`
+			Endpoint     string `json:"endpoint"`
+			Provider     string `json:"provider"`
+			Region       string `json:"region"`
 		} `json:"attributes"`
 	} `json:"data"`
 }
